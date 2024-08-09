7bf6c106ced43502ab788c3153f59513a2ca7920
youssef <youssefahmedabdelkader25@gmail.com>
2024-08-09 17:53:32 +0300

    task 1

---
+++ web_dynamic/0-hbnb.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+""" Starts a Flash Web Application """
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from os import environ
+from flask import Flask, render_template
+import os
+from uuid import uuid4
+app = Flask(__name__)
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
+
+
+@app.route('/0-hbnb/', strict_slashes=False)
+def hbnb():
+    """ HBNB is alive! """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    st_ct = []
+    cache_id = uuid4()
+
+    for state in states:
+        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
+
+    amenities = storage.all(Amenity).values()
+    amenities = sorted(amenities, key=lambda k: k.name)
+
+    places = storage.all(Place).values()
+    places = sorted(places, key=lambda k: k.name)
+
+    if not os.path.exists('100-hbnb.html'):
+        return render_template('8-hbnb.html',
+                        states=st_ct,
+                        amenities=amenities,
+                        places=places,
+                        cache_id=cache_id)
+
+    else:
+        return render_template('100-hbnb.html',
+                            states=st_ct,
+                            amenities=amenities,
+                            places=places,
+                            cache_id=cache_id)
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
